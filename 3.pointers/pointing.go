71dafada3e56f058980738c329b4cb2fb3ef7dff
Chandra5468 <agirchandra@gmail.com>
2025-11-09 09:41:01 +0530

    pointers

---
+++ 3.pointers/pointing.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+	Know how stack and heap memory work with pointers in go.
+
+	If you create a variable inside a function, it is created on the stack.
+	If you pass the address of that variable to another function, the variable is moved to the stack.
+	If you return the pointer it will be moved to the heap.
+*/
